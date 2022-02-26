9578ae0783f935c7f30b24a537b86ee41aa661ff
Mckenzie Irwin <irwinmck@gmail.com>
2022-02-25 22:51:06 -0500

    setup eslint

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
+
+// Create App.
 const app = express();
 
 // Routers
 const tourRouter = require('./routes/tours.routes');
 const userRouter = require('./routes/users.routes');
 
-// Middleware
+// Middleware.
 app.use(express.json());
-app.use(morgan('dev'));
 app.use(express.static(`${__dirname}/public`));
 
-// Mounting Routes
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Mounting Routes.
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const app = require('./app');
 
-// Run Server
-const port = 3000;
+// Run Server.
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`app running on port: ${port}`);
 });
