29d1ae55f9891c3bf1526dbe06caf7c3b38c0f53
Mckenzie Irwin <irwinmck@gmail.com>
2022-02-26 00:11:20 -0500

    added mongoose and created base tour model

--- controllers/tour.controller.js
+++ controllers/tour.controller.js
@@ -1,11 +1,23 @@
-const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+const Tour = require('../models/tour.model');
 
 exports.getAllTours = (req, res) => {};
-exports.createTour = (req, res) => {};
+
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+};
 
 exports.getTour = (req, res) => {};
 exports.updateTour = (req, res) => {};
--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 const app = require('./app');
+
+// DB Connection.
+mongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connection successful!');
+  });
 
 // Run Server.
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`app running on port: ${port}`);
+  console.log(`Maks Tours is running on port: ${port}!`);
 });
