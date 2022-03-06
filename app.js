b47dfae98c52163d81c4051e2cbac9a579b52f6e
Mckenzie Irwin <irwinmck@gmail.com>
2022-03-06 15:26:32 -0500

    added security middleware

--- app.js
+++ app.js
@@ -1,24 +1,49 @@
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 // Internal Imports.
 const AppError = require('./utils/AppError');
 const { baseErrorHandler } = require('./utils/error-handlers');
 
-// Create App.
-const app = express();
-
 // Routers
 const tourRouter = require('./routes/tours.routes');
 const userRouter = require('./routes/users.routes');
 
-// Middleware.
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
+// Create App.
+const app = express();
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
+// Middleware.
+app.use(helmet());
+app.use(
+  '/api',
+  rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests have been sent! Try again later!',
+  })
+);
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+app.use(express.json({ limit: '10kb' }));
+app.use(mongoSanitize());
+app.use(xss());
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+app.use(express.static(`${__dirname}/public`));
 
 // Mounting Routes.
 app.use('/api/v1/tours', tourRouter);
