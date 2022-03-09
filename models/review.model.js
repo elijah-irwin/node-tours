866b3bae5a174478c521f07a115faff536975962
Mckenzie Irwin <irwinmck@gmail.com>
2022-03-08 21:10:45 -0500

    added logic to calc tour averages and update them on crud ops

--- models/review.model.js
+++ models/review.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tour.model');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -25,12 +26,49 @@ const reviewSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+// Indexes
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
+// Statics
+reviewSchema.statics.calcAvgRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    { $match: { tour: tourId } },
+    {
+      $group: {
+        _id: '$tour',
+        numRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsAverage: stats.length ? stats[0].avgRating : 4.5,
+    ratingsQuantity: stats.length ? stats[0].numRating : 0,
+  });
+};
+
+// Middlewares
 reviewSchema.pre(/^find/, function (next) {
   this.populate({
     path: 'user',
     select: 'name photo email',
   });
   next();
+});
+
+reviewSchema.post('save', function () {
+  this.constructor.calcAvgRatings(this.tour);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.r = await this.clone().findOne();
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  // await this.findOne() does NOT work here as the query has already executed.
+  await this.r.constructor.calcAvgRatings(this.r.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
